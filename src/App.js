e2e2067cb315e343f3b9440789407155813128cb
Ensar <ensarhamzic01@gmail.com>
2022-09-09 19:45:01 +0200

    different content depending on auth state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import classes from "./App.module.css"
 import { Routes, Route } from "react-router-dom"
 import Navigation from "./components/Navigation/Navigation"
+import { useSelector } from "react-redux"
+import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
+  const isAuth = useSelector((state) => state.auth.isAuth)
+
   return (
-    <div className={classes.app}>
-      <Navigation />
-      <Routes></Routes>
-    </div>
+    <>
+      {isAuth && (
+        <div className={classes.authApp}>
+          <Navigation />
+          <Routes></Routes>
+        </div>
+      )}
+
+      {!isAuth && (
+        <div>
+          <div className={classes.backgroundDiv} />
+          <Navbar />
+        </div>
+      )}
+    </>
   )
 }
 
