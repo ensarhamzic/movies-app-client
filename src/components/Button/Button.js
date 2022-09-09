419965858901dd2290525d1c58106c25bd687d92
Ensar <ensarhamzic01@gmail.com>
2022-09-09 19:45:05 +0200

    cahnges

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,13 @@
 import React from "react"
 import classes from "./Button.module.css"
 
-const Button = ({ className, content }) => {
-  const buttonClasses = `${className} ${classes.button}`
-  return <button className={buttonClasses}>{content}</button>
+const Button = ({ className, content, onClick }) => {
+  const buttonClasses = `${classes.button} ${className}`
+  return (
+    <button className={buttonClasses} onClick={onClick}>
+      {content}
+    </button>
+  )
 }
 
 export default Button
