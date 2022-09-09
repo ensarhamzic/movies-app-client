1334119f659f7e6aab51a9bf0eb0c4acaae2d661
Ensar <ensarhamzic01@gmail.com>
2022-09-10 00:11:48 +0200

    new pages added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { Routes, Route } from "react-router-dom"
 import Navigation from "./components/Navigation/Navigation"
 import { useSelector } from "react-redux"
 import Navbar from "./components/Navbar/Navbar"
+import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
+import "react-notifications/lib/notifications.css"
+import { NotificationContainer } from "react-notifications"
 
 const App = () => {
   const isAuth = useSelector((state) => state.auth.isAuth)
 
   return (
     <>
+      <NotificationContainer />
       {isAuth && (
         <div className={classes.authApp}>
           <Navigation />
@@ -20,6 +25,10 @@ const App = () => {
         <div>
           <div className={classes.backgroundDiv} />
           <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
         </div>
       )}
     </>
