2a58b16640b677084bcc857b497e29890b84bcdb
Ensar <ensarhamzic01@gmail.com>
2022-09-09 19:44:44 +0200

    configured redux store

---
+++ src/store/auth-slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  token: null,
+  isAuth: false,
+  user: {
+    id: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+  },
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload.token
+      state.isAuth = true
+      const { id, firstName, lastName, email } = action.payload.user
+      state.user = {
+        id,
+        firstName,
+        lastName,
+        email,
+      }
+      localStorage.setItem("token", action.payload.token)
+    },
+    logout: (state) => {
+      state.token = initialState.token
+      state.isAuth = initialState.isAuth
+      state.user = { ...initialState.user }
+      localStorage.removeItem("token")
+    },
+  },
+})
+
+export const authActions = authSlice.actions
+export default authSlice.reducer
