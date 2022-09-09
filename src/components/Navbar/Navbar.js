d0ac5f6622d59abe6812f2c281d53318ec147881
Ensar <ensarhamzic01@gmail.com>
2022-09-10 00:13:22 +0200

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import Button from "../Button/Button"
+import { NavLink } from "react-router-dom"
 import Logo from "../Logo/Logo"
 import classes from "./Navbar.module.css"
 
@@ -9,8 +8,18 @@ const Navbar = () => {
     <div className={classes.navbar}>
       <Logo />
       <div className={classes.actions}>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        <NavLink
+          className={(nav) => (nav.isActive ? classes.active : "")}
+          to="/login"
+        >
+          Login
+        </NavLink>
+        <NavLink
+          className={(nav) => (nav.isActive ? classes.active : "")}
+          to="/register"
+        >
+          Register
+        </NavLink>
       </div>
     </div>
   )
