96d0af74c59abf573b4bcdd565c5845e5673df64
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:36:00 +0200

    item can be button

--- src/components/NavigationItem/NavigationItem.js
+++ src/components/NavigationItem/NavigationItem.js
@@ -2,22 +2,39 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 import classes from "./NavigationItem.module.css"
 
-const NavigationItem = ({ location, title, icon, alt, className }) => {
-  const wrapperClasses = `${classes.wrapper} ${className || ""}`
-  return (
-    <NavLink
-      to={location}
-      className={(nav) =>
-        `${classes.link} ${nav.isActive ? classes.active : ""}`
-      }
-    >
-      <div className={wrapperClasses}>
-        <div className={classes.item}>
-          <img className={classes.img} src={icon} alt={alt} />
-          <p className={classes.title}>{title}</p>
+const NavigationItem = ({
+  location,
+  title,
+  icon,
+  alt,
+  className,
+  button,
+  onClick,
+}) => {
+  if (!button)
+    return (
+      <NavLink
+        to={location}
+        className={(nav) =>
+          `${classes.link} ${nav.isActive ? classes.active : ""} ${className}`
+        }
+      >
+        <div className={classes.wrapper}>
+          <div className={classes.item}>
+            <img className={classes.img} src={icon} alt={alt} />
+            <p className={classes.title}>{title}</p>
+          </div>
         </div>
+      </NavLink>
+    )
+
+  return (
+    <div className={classes.wrapper} onClick={onClick}>
+      <div className={classes.item}>
+        <img className={classes.img} src={icon} alt={alt} />
+        <p className={classes.title}>{title}</p>
       </div>
-    </NavLink>
+    </div>
   )
 }
 
