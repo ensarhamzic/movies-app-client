501d9bc80f9c8ec982a3464c323557f70e8ce23d
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:33:21 +0200

    accepts custom classes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,17 @@
 import React from "react"
 // import classes from "./Form.module.css"
 
-const Form = ({ children, onSubmit }) => {
+const Form = ({ children, onSubmit, className }) => {
   const formSubmitHandler = (e) => {
     e.preventDefault()
     onSubmit()
   }
 
-  return <form onSubmit={formSubmitHandler}>{children}</form>
+  return (
+    <form onSubmit={formSubmitHandler} className={className}>
+      {children}
+    </form>
+  )
 }
 
 export default Form
