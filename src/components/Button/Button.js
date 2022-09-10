9e03fdd498bd740a73b2ba6ded27d31530f2f112
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:33:13 +0200

    can be disabled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,10 @@
 import React from "react"
 import classes from "./Button.module.css"
 
-const Button = ({ className, content, onClick }) => {
+const Button = ({ className, content, onClick, disabled = false }) => {
   const buttonClasses = `${classes.button} ${className}`
   return (
-    <button className={buttonClasses} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
       {content}
     </button>
   )
