aa5213cf47d920fe3b4d6f1eb55b5df4ef836931
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:37:25 +0200

    manages http requests

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,3 +1,51 @@
 import { useState, useCallback } from "react"
 
 const API_URL = process.env.REACT_APP_API_URL
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const sendRequest = useCallback(
+    async ({
+      url,
+      method,
+      data,
+      token,
+      errorMessage,
+      contentType = "application/json",
+    }) => {
+      const fullUrl = `${API_URL}${url}`
+      setIsLoading(true)
+      setError(null)
+      let fetchConfig = {
+        method,
+        headers: {
+          "Content-Type": contentType,
+        },
+      }
+      if (data) fetchConfig.body = JSON.stringify(data)
+      if (token)
+        fetchConfig.headers = {
+          ...fetchConfig.headers,
+          Authorization: `Bearer ${token}`,
+        }
+      let response = null
+      const serverResponse = await fetch(fullUrl, fetchConfig)
+      response = await serverResponse.json()
+      if (!serverResponse.ok) {
+        setError(errorMessage || response.message)
+        setIsLoading(false)
+        return null
+      }
+      setError(null)
+      setIsLoading(false)
+      return response
+    },
+    []
+  )
+
+  return { isLoading, error, sendRequest }
+}
+
+export default useHttp
