bf6c253e418349e4b31df1861ffe377da64f7fd5
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:34:08 +0200

    can be disabled

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -9,6 +9,7 @@ const Input = ({
   error,
   submitted,
   className,
+  disabled = false,
 }) => {
   const [blurred, setBlurred] = useState(false)
   const blurHandler = () => {
@@ -30,6 +31,7 @@ const Input = ({
         onChange={onChange}
         onFocus={focusHandler}
         onBlur={blurHandler}
+        disabled={disabled}
       />
     </div>
   )
