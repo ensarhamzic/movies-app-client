74324472903120d41e807aeda52729125dcf9037
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:35:08 +0200

    register page

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,8 +1,25 @@
 import React from "react"
 import classes from "./Register.module.css"
+import Card from "../../components/Card/Card"
+import logoIcon from "../../images/logo.svg"
+import { Link } from "react-router-dom"
+import RegisterForm from "../../components/RegisterForm/RegisterForm"
 
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <Card className={classes.card}>
+      <div className={classes.header}>
+        <img src={logoIcon} alt="logo" />
+        <p>Sign up for movlib</p>
+      </div>
+      <RegisterForm />
+      <div className={classes.footer}>
+        <p>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
+    </Card>
+  )
 }
 
 export default Register
