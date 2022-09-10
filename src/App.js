a6ed67cd5f8aad4465beb9cf8df951826bfa05a9
Ensar <ensarhamzic01@gmail.com>
2022-09-10 22:33:02 +0200

    new routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import classes from "./App.module.css"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Navigation from "./components/Navigation/Navigation"
 import { useSelector } from "react-redux"
 import Navbar from "./components/Navbar/Navbar"
@@ -7,17 +7,27 @@ import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import "react-notifications/lib/notifications.css"
 import { NotificationContainer } from "react-notifications"
+import AddCollection from "./pages/AddCollection/AddCollection"
 
 const App = () => {
   const isAuth = useSelector((state) => state.auth.isAuth)
 
   return (
     <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={isAuth ? "/library" : "/login"} />}
+        />
+      </Routes>
+
       <NotificationContainer />
       {isAuth && (
         <div className={classes.authApp}>
           <Navigation />
-          <Routes></Routes>
+          <Routes>
+            <Route path="/add-collection" element={<AddCollection />} />
+          </Routes>
         </div>
       )}
 
