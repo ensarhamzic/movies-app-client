2f5bd0062c5cedf845b1d9591ea810c5ab7c9b87
Ensar <ensarhamzic01@gmail.com>
2022-09-12 20:53:13 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,82 @@ import Register from "./pages/Register/Register"
 import "react-notifications/lib/notifications.css"
 import { NotificationContainer } from "react-notifications"
 import AddCollection from "./pages/AddCollection/AddCollection"
+import AddItems from "./pages/AddItems/AddItems"
+import React, { useEffect, useState } from "react"
+import useHttp from "./hooks/use-http"
+import { useDispatch } from "react-redux"
+import { authActions } from "./store/auth-slice"
+import Spinner from "./components/Spinner/Spinner"
+import { collectionsActions } from "./store/collections-slice"
+import { favoritesActions } from "./store/favorites-slice"
 
 const App = () => {
+  const dispatch = useDispatch()
   const isAuth = useSelector((state) => state.auth.isAuth)
+  const authToken = useSelector((state) => state.auth.token)
+  const [loggingIn, setLoggingIn] = useState(false)
+  const { sendRequest: verifyToken } = useHttp()
+  const { sendRequest: getCollections } = useHttp()
+  const { sendRequest: getFavorites } = useHttp()
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      ;(async () => {
+        setLoggingIn(true)
+        const response = await verifyToken({
+          url: "/users/verify",
+          method: "POST",
+          token,
+        })
+
+        if (!response) {
+          localStorage.removeItem("token")
+          setLoggingIn(false)
+          return
+        }
+
+        dispatch(authActions.login({ token, user: response.user }))
+        setLoggingIn(false)
+      })()
+    }
+  }, [dispatch, verifyToken])
+
+  useEffect(() => {
+    if (isAuth) {
+      ;(async () => {
+        const response = await getCollections({
+          url: "/collections",
+          method: "GET",
+          token: authToken,
+        })
+
+        if (!response) return
+
+        dispatch(collectionsActions.loadCollections({ collections: response }))
+      })()
+    }
+  }, [isAuth, getCollections, authToken, dispatch])
+
+  useEffect(() => {
+    if (isAuth) {
+      ;(async () => {
+        const response = await getFavorites({
+          url: "/users/favorites",
+          method: "GET",
+          token: authToken,
+        })
+
+        if (!response) return
+
+        dispatch(favoritesActions.loadFavorites({ favorites: response }))
+      })()
+    }
+  }, [isAuth, getFavorites, authToken, dispatch])
 
   return (
     <>
+      <Spinner loading={loggingIn} size={150} />
       <Routes>
         <Route
           path="/"
@@ -27,6 +97,7 @@ const App = () => {
           <Navigation />
           <Routes>
             <Route path="/add-collection" element={<AddCollection />} />
+            <Route path="/add-items" element={<AddItems />} />
           </Routes>
         </div>
       )}
