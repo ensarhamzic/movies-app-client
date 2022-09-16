5283935f4d224786d2d5457c8613f0aa3c1206b7
Ensar <ensarhamzic01@gmail.com>
2022-09-16 19:39:16 +0200

    extracted component

--- src/components/CollectionHeader/CollectionHeader.js
+++ src/components/CollectionHeader/CollectionHeader.js
@@ -1,8 +1,175 @@
-import React from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import classes from "./CollectionHeader.module.css"
+import Search from "../Search/Search"
+import Select from "../Select/Select"
+import Button from "../Button/Button"
+import LetterFilter from "../LetterFilter/LetterFilter"
+import { FaArrowUp, FaArrowDown } from "react-icons/fa"
+import { BsFilter } from "react-icons/bs"
+import useInput from "../../hooks/use-input"
+import { useSelector } from "react-redux"
 
-const CollectionHeader = () => {
-  return <div>CollectionHeader</div>
+const showTypeOptions = [
+  { id: "Cover", name: "Cover" },
+  { id: "List", name: "List" },
+  { id: "Summary", name: "Summary" },
+]
+
+const sortingOptions = [
+  { id: "Title", name: "Title" },
+  { id: "Release Date", name: "Release Date" },
+  { id: "Rating", name: "Rating" },
+]
+
+const CollectionHeader = ({
+  onApplyFilter,
+  loadingMovies,
+  movies,
+  filters,
+  collection,
+  onCollectionChange,
+  showType,
+  onShowTypeChange,
+  onFiltersClick,
+  choosenCollection,
+  sorting,
+  onSortingChange,
+  onSortOrderChange,
+  sortAscending,
+  hideFilters,
+}) => {
+  const collections = useSelector((state) => state.collections.data)
+  const [choosenLetter, setChoosenLetter] = useState("ALL")
+
+  const [search, setSearch] = useState(false)
+
+  const {
+    value: searchQuery,
+    onChange: onSearchQueryChange,
+    setValue: setSearchQuery,
+  } = useInput()
+
+  const filterByLetter = useCallback(
+    (movs) => {
+      if (choosenLetter === "ALL") {
+        onApplyFilter([...movs])
+      } else if (choosenLetter === "#") {
+        onApplyFilter(movs.filter((m) => m.title.charAt(0).match(/[^a-zA-z]/)))
+      } else {
+        onApplyFilter(movs.filter((m) => m.title.charAt(0) === choosenLetter))
+      }
+    },
+    [choosenLetter, onApplyFilter]
+  )
+
+  useEffect(() => {
+    if (!choosenCollection || loadingMovies) return
+    filterByLetter(movies)
+  }, [choosenCollection, loadingMovies, filterByLetter, movies])
+
+  const collectionChanged = (event) => {
+    onCollectionChange(event)
+    setChoosenLetter("ALL")
+
+    setSearch(false)
+    setSearchQuery("")
+  }
+
+  useEffect(() => {
+    if (search) return
+    if (filters) {
+      let unfilteredMovies = [...movies]
+      unfilteredMovies = unfilteredMovies.filter(
+        (m) =>
+          m.vote_average >= filters.ratings.from &&
+          m.vote_average <= filters.ratings.to
+      )
+      if (filters.years.from)
+        unfilteredMovies = unfilteredMovies.filter(
+          (m) => m.release_date >= filters.years.from
+        )
+      if (filters.years.to) {
+        unfilteredMovies = unfilteredMovies.filter(
+          (m) => m.release_date <= filters.years.to
+        )
+      }
+      if (filters.genres.length > 0)
+        unfilteredMovies = unfilteredMovies.filter((m) => {
+          const movieGenres = m.genres.map((g) => g.id)
+          const filteredGenres = filters.genres.map((g) => g.value)
+          return movieGenres.some((r) => filteredGenres.indexOf(r) >= 0)
+        })
+
+      filterByLetter(unfilteredMovies)
+    } else {
+      filterByLetter(movies)
+      hideFilters()
+    }
+  }, [filterByLetter, filters, hideFilters, movies, search])
+
+  const searchHandler = (movies) => {
+    onApplyFilter(movies)
+    setSearch(true)
+    hideFilters()
+  }
+
+  const closeSearch = () => {
+    setSearch(false)
+    setSearchQuery("")
+  }
+
+  return (
+    <div className={classes.header}>
+      <Search
+        movies={movies}
+        onSearch={searchHandler}
+        active={search}
+        onClose={closeSearch}
+        value={searchQuery}
+        onChange={onSearchQueryChange}
+      />
+      <div className={classes.headerActions}>
+        <Select
+          value={collection}
+          onChange={collectionChanged}
+          options={collections}
+          className={classes.collectionSelect}
+          placeholder="--- Choose Collection ---"
+        />
+        <Select
+          value={showType}
+          onChange={onShowTypeChange}
+          options={showTypeOptions}
+          className={classes.showTypeSelect}
+        />
+        <div className={classes.sorting}>
+          <Select
+            value={sorting}
+            onChange={onSortingChange}
+            options={sortingOptions}
+            className={classes.sortingSelect}
+          />
+          <Button onClick={onSortOrderChange}>
+            {sortAscending && <FaArrowUp />}
+            {!sortAscending && <FaArrowDown />}
+          </Button>
+        </div>
+        {!search && (
+          <Button className={classes.filterBtn} onClick={onFiltersClick}>
+            <BsFilter />
+            Filters
+          </Button>
+        )}
+      </div>
+      {!search && (
+        <LetterFilter
+          className={classes.letters}
+          onChange={setChoosenLetter}
+          choosenLetter={choosenLetter}
+        />
+      )}
+    </div>
+  )
 }
 
 export default CollectionHeader
--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,69 +1,53 @@
-import React, { useCallback } from "react"
+import React from "react"
 import { useEffect } from "react"
 import { useState } from "react"
 import { useSelector } from "react-redux"
 import AuthPageWrapper from "../../components/AuthPageWrapper/AuthPageWrapper"
-import LetterFilter from "../../components/LetterFilter/LetterFilter"
 import Movies from "../../components/Movies/Movies"
-import Select from "../../components/Select/Select"
 import Spinner from "../../components/Spinner/Spinner"
 import useHttp from "../../hooks/use-http"
 import useInput from "../../hooks/use-input"
 import classes from "./Library.module.css"
-import Button from "../../components/Button/Button"
-import { FaArrowUp, FaArrowDown } from "react-icons/fa"
-import { BsFilter } from "react-icons/bs"
 import Filters from "../../components/Filters/Filters"
-import Search from "../../components/Search/Search"
+import CollectionHeader from "../../components/CollectionHeader/CollectionHeader"
+import { useCallback } from "react"
 
 const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY
 
-const showTypeOptions = [
-  { id: "Cover", name: "Cover" },
-  { id: "List", name: "List" },
-  { id: "Summary", name: "Summary" },
-]
-
-const sortingOptions = [
-  { id: "Title", name: "Title" },
-  { id: "Release Date", name: "Release Date" },
-  { id: "Rating", name: "Rating" },
-]
-
 const Library = () => {
   const collections = useSelector((state) => state.collections.data)
-  const [choosenLetter, setChoosenLetter] = useState("ALL")
-  const [movies, setMovies] = useState([])
-  const [filteredMovies, setFilteredMovies] = useState([])
-  const [loadingMovies, setLoadingMovies] = useState(false)
   const [filtersShowed, setFiltersShowed] = useState(false)
   const [filtersHiding, setFiltersHiding] = useState(false)
   const [isInitial, setIsInitial] = useState(true)
-  const [filters, setFilters] = useState(null)
-  const [search, setSearch] = useState(false)
-  const { sendRequest: getMovieDetails } = useHttp()
-  const {
-    value: collection,
-    onChange: onCollectionChange,
-    setValue: setCollection,
-  } = useInput()
+
   const { value: showType, onChange: onShowTypeChange } = useInput(
     null,
     "Cover"
   )
   const { value: sorting, onChange: onSortingChange } = useInput(null, "Title")
-  const [sortAscending, setSortAscendig] = useState(true)
+  const [sortAscending, setSortAscending] = useState(true)
+  const [filters, setFilters] = useState(null)
+
+  const {
+    value: collection,
+    onChange: onCollectionChange,
+    setValue: setCollection,
+  } = useInput()
+
+  const [movies, setMovies] = useState([])
+  const [loadingMovies, setLoadingMovies] = useState(false)
+  const [filteredMovies, setFilteredMovies] = useState([])
+
+  const { sendRequest: getMovieDetails } = useHttp()
 
   let choosenCollection = null
   if (collection) {
     choosenCollection = collections.find((c) => c.id === parseInt(collection))
   }
 
-  const {
-    value: searchQuery,
-    onChange: onSearchQueryChange,
-    setValue: setSearchQuery,
-  } = useInput()
+  const sortOrderChange = () => {
+    setSortAscending((prevSort) => !prevSort)
+  }
 
   useEffect(() => {
     if (collections.length > 0 && isInitial) {
@@ -91,41 +75,6 @@ const Library = () => {
     })()
   }, [getMovieDetails, choosenCollection, collection])
 
-  const filterByLetter = useCallback(
-    (movs) => {
-      if (choosenLetter === "ALL") {
-        setFilteredMovies([...movs])
-      } else if (choosenLetter === "#") {
-        setFilteredMovies(
-          movs.filter((m) => m.title.charAt(0).match(/[^a-zA-z]/))
-        )
-      } else {
-        setFilteredMovies(
-          movs.filter((m) => m.title.charAt(0) === choosenLetter)
-        )
-      }
-    },
-    [choosenLetter]
-  )
-
-  useEffect(() => {
-    if (!choosenCollection || loadingMovies) return
-    filterByLetter(movies)
-  }, [choosenCollection, loadingMovies, filterByLetter, movies])
-
-  const collectionChanged = (event) => {
-    onCollectionChange(event)
-    setChoosenLetter("ALL")
-    setFilters(null)
-    setFiltersShowed(false)
-    setSearch(false)
-    setSearchQuery("")
-  }
-
-  const sortOrderChange = () => {
-    setSortAscendig((prevSort) => !prevSort)
-  }
-
   const filtersButtonClick = () => {
     if (filtersShowed) {
       setFiltersHiding(true)
@@ -138,48 +87,15 @@ const Library = () => {
     }
   }
 
-  useEffect(() => {
-    if (search) return
-    if (filters) {
-      let unfilteredMovies = [...movies]
-      unfilteredMovies = unfilteredMovies.filter(
-        (m) =>
-          m.vote_average >= filters.ratings.from &&
-          m.vote_average <= filters.ratings.to
-      )
-      if (filters.years.from)
-        unfilteredMovies = unfilteredMovies.filter(
-          (m) => m.release_date >= filters.years.from
-        )
-      if (filters.years.to) {
-        unfilteredMovies = unfilteredMovies.filter(
-          (m) => m.release_date <= filters.years.to
-        )
-      }
-      if (filters.genres.length > 0)
-        unfilteredMovies = unfilteredMovies.filter((m) => {
-          const movieGenres = m.genres.map((g) => g.id)
-          const filteredGenres = filters.genres.map((g) => g.value)
-          return movieGenres.some((r) => filteredGenres.indexOf(r) >= 0)
-        })
-
-      filterByLetter(unfilteredMovies)
-    } else {
-      filterByLetter(movies)
-      setFiltersShowed(false)
-    }
-  }, [filters, movies, filterByLetter, search])
-
-  const searchHandler = (movies) => {
-    setFilteredMovies(movies)
-    setSearch(true)
+  const collectionChangeHandler = (event) => {
+    onCollectionChange(event)
+    setFilters(null)
     setFiltersShowed(false)
   }
 
-  const closeSearch = () => {
-    setSearch(false)
-    setSearchQuery("")
-  }
+  const hideFilters = useCallback(() => {
+    setFiltersShowed(false)
+  }, [])
 
   return (
     <AuthPageWrapper className={classes.content}>
@@ -189,59 +105,23 @@ const Library = () => {
           filtersShowed ? classes.reduced : ""
         }`}
       >
-        <div className={classes.header}>
-          <Search
-            movies={movies}
-            onSearch={searchHandler}
-            active={search}
-            onClose={closeSearch}
-            value={searchQuery}
-            onChange={onSearchQueryChange}
-          />
-          <div className={classes.headerActions}>
-            <Select
-              value={collection}
-              onChange={collectionChanged}
-              options={collections}
-              className={classes.collectionSelect}
-              placeholder="--- Choose Collection ---"
-            />
-            <Select
-              value={showType}
-              onChange={onShowTypeChange}
-              options={showTypeOptions}
-              className={classes.showTypeSelect}
-            />
-            <div className={classes.sorting}>
-              <Select
-                value={sorting}
-                onChange={onSortingChange}
-                options={sortingOptions}
-                className={classes.sortingSelect}
-              />
-              <Button onClick={sortOrderChange}>
-                {sortAscending && <FaArrowUp />}
-                {!sortAscending && <FaArrowDown />}
-              </Button>
-            </div>
-            {!search && (
-              <Button
-                className={classes.filterBtn}
-                onClick={filtersButtonClick}
-              >
-                <BsFilter />
-                Filters
-              </Button>
-            )}
-          </div>
-          {!search && (
-            <LetterFilter
-              className={classes.letters}
-              onChange={setChoosenLetter}
-              choosenLetter={choosenLetter}
-            />
-          )}
-        </div>
+        <CollectionHeader
+          collection={collection}
+          choosenCollection={choosenCollection}
+          movies={movies}
+          onApplyFilter={setFilteredMovies}
+          onCollectionChange={collectionChangeHandler}
+          loadingMovies={loadingMovies}
+          filters={filters}
+          showType={showType}
+          onShowTypeChange={onShowTypeChange}
+          onFiltersClick={filtersButtonClick}
+          sorting={sorting}
+          onSortingChange={onSortingChange}
+          onSortOrderChange={sortOrderChange}
+          sortAscending={sortAscending}
+          hideFilters={hideFilters}
+        />
         <div className={classes.movies}>
           <Movies
             movies={filteredMovies}
