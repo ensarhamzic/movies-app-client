dd49efa08c19e94ecd8e0a41d2e652c93a20aaa7
Ensar Hamzic <ensar_hamzic@hotmail.com>
2022-09-22 21:57:06 +0200

    delete account requires password input

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -8,17 +8,28 @@ import { useSelector, useDispatch } from "react-redux"
 import Spinner from "../../components/Spinner/Spinner"
 import NotificationManager from "react-notifications/lib/NotificationManager"
 import useHttp from "../../hooks/use-http"
+import Input from "../../components/Input/Input"
+import { useEffect } from "react"
+import useInput from "../../hooks/use-input"
 
 const Profile = () => {
   const token = useSelector((state) => state.auth.token)
   const dispatch = useDispatch()
+  const { value: passwordValue, onChange: onPasswordChange } = useInput()
 
-  const { isLoading: deletingAccount, sendRequest: deleteAccount } = useHttp()
+  const {
+    error: deleteAccountError,
+    isLoading: deletingAccount,
+    sendRequest: deleteAccount,
+  } = useHttp()
 
   const deleteAccountHandler = async () => {
     const response = await deleteAccount({
       url: "/users",
       method: "DELETE",
+      data: {
+        password: passwordValue,
+      },
       token,
     })
 
@@ -28,20 +39,33 @@ const Profile = () => {
     NotificationManager.success(response.message, "Success!", 2000)
   }
 
+  useEffect(() => {
+    deleteAccountError &&
+      NotificationManager.error(deleteAccountError, "Error!", 2000)
+  }, [deleteAccountError])
+
   return (
     <AuthPageWrapper className={classes.wrapper}>
       <ProfileButton />
       <Spinner loading={deletingAccount} />
       <h1 className={classes.header}>Profile settings</h1>
       <hr />
-      <CollapsingContent title="Delete Account" contentClass={classes.collapse}>
+      <CollapsingContent title="Delete Account">
         <p className={classes.warning}>Account will be deleted permanently!</p>
-        <Button
-          className={classes.deleteAccountButton}
-          onClick={deleteAccountHandler}
-        >
-          Delete My Account
-        </Button>
+        <div className={classes.deleteContent}>
+          <Input
+            type="password"
+            placeholder="Password"
+            onChange={onPasswordChange}
+            value={passwordValue}
+          />
+          <Button
+            className={classes.deleteAccountButton}
+            onClick={deleteAccountHandler}
+          >
+            Delete My Account
+          </Button>
+        </div>
       </CollapsingContent>
     </AuthPageWrapper>
   )
