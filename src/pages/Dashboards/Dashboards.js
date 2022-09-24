1eed5eb266fbf3b359588553ecc4c6433b24adb2
Ensar <ensarhamzic01@gmail.com>
2022-09-24 11:34:55 +0200

    dashboard charts

--- src/pages/Dashboards/Dashboards.js
+++ src/pages/Dashboards/Dashboards.js
@@ -16,11 +16,14 @@ import { useSelector } from "react-redux"
 import useHttp from "../../hooks/use-http"
 import Spinner from "../../components/Spinner/Spinner"
 import Card from "../../components/Card/Card"
+import CollapsingContent from "../../components/CollapsingContent/CollapsingContent"
+import ProfileButton from "../../components/ProfileButton/ProfileButton"
+
 const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-const genresOptions = {
+const barChartOptions = {
   responsive: true,
   plugins: {
     legend: {
@@ -28,34 +31,47 @@ const genresOptions = {
     },
     title: {
       display: false,
-      text: "Movies By Genres",
-    },
-  },
-}
-const ratingsOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-    title: {
-      display: false,
-      text: "Movies By Rating",
+      text: "",
     },
   },
 }
 
+const getAllMovies = (collections) => {
+  let allMovies = []
+  collections.forEach((c) => {
+    allMovies = [...allMovies, ...c.movies]
+  })
+  return allMovies
+}
+
+const getAllUniqueMovies = (movies) => {
+  const uniqueMovies = [...new Set(movies.map((movie) => movie.id))].map(
+    (m) => {
+      return { id: m }
+    }
+  )
+
+  return uniqueMovies
+}
+
 const Dashboards = () => {
   const collections = useSelector((state) => state.collections.data)
+  const totalMovies = getAllMovies(collections)
+  const totalUniqueMovies = getAllUniqueMovies(totalMovies)
+
   const collectionsOptions = collections.map((c) => {
     return { label: c.name, value: c.id }
   })
-  collectionsOptions.unshift({ label: "All Collections", value: 0 })
+  collectionsOptions.unshift({
+    label: "All Collections (Only unique movies)",
+    value: 0,
+  })
   const [collection, setCollection] = useState(collectionsOptions[0])
 
   const genres = useSelector((state) => state.genres.data)
   const [genresChartData, setGenresChartData] = useState(null)
   const [ratingsChartData, setRatingsChartData] = useState(null)
+  const [releaseYearChartData, setReleaseYearChartData] = useState(null)
 
   const { sendRequest: getMovieDetails } = useHttp()
   const [movies, setMovies] = useState([])
@@ -101,6 +117,34 @@ const Dashboards = () => {
   }, [movies, collection])
 
   useEffect(() => {
+    // prepares data for bar chart that represents movies by release year
+    let releaseYears = movies.map((m) => m.release_date.substring(0, 4))
+    releaseYears = [...new Set(releaseYears)]
+    releaseYears.sort((a, b) => a - b)
+
+    const labels = releaseYears.map((y) => (y ? y : "N/A"))
+
+    const data = releaseYears.map((y) => {
+      return movies.reduce((prev, curr) => {
+        const releaseYear = curr.release_date.substring(0, 4)
+        if (releaseYear === y) return prev + 1
+        return prev
+      }, 0)
+    })
+
+    setReleaseYearChartData({
+      labels,
+      datasets: [
+        {
+          label: collection.label,
+          data,
+          backgroundColor: "#0e4f59",
+        },
+      ],
+    })
+  }, [movies, collection])
+
+  useEffect(() => {
     if (genres.length === 0) return
     const labels = genres.map((g) => g.name)
     let data = labels.map((l) => 0)
@@ -139,9 +183,7 @@ const Dashboards = () => {
             .movies,
         ]
       } else {
-        collections.forEach((c) => {
-          allMovies = [...allMovies, ...c.movies]
-        })
+        allMovies = getAllUniqueMovies(getAllMovies(collections))
       }
 
       for (const movie of allMovies) {
@@ -160,31 +202,63 @@ const Dashboards = () => {
 
   return (
     <AuthPageWrapper className={classes.wrapper}>
+      <ProfileButton />
       <Spinner loading={loadingMovies} />
       <h1 className={classes.header}>Dashboards</h1>
       <hr />
-      <p className={classes.selectTitle}>Choose Collection</p>
-      <ReactSelect
-        options={collectionsOptions}
-        value={collection}
-        onChange={setCollection}
-      />
-      {genresChartData && (
+
+      <div className={classes.chooseCollectionWrapper}>
+        <p className={classes.selectTitle}>Choose Collection</p>
+        <ReactSelect
+          options={collectionsOptions}
+          value={collection}
+          onChange={setCollection}
+          className={classes.chooseCollection}
+        />
+      </div>
+      <div className={classes.overallDetails}>
         <Card className={classes.card}>
-          <p className={classes.chartTitle}>Movies By Genres</p>
-          <Bar options={genresOptions} data={genresChartData} />
+          <p>Total movies in all collections:</p>
+          <p className={classes.moviesNum}>{totalMovies.length}</p>
         </Card>
-      )}
-      {ratingsChartData && (
         <Card className={classes.card}>
-          <p className={classes.chartTitle}>Movies By Rating</p>
-          <Bar
-            options={ratingsOptions}
-            data={ratingsChartData}
-            style={{ height: "300px !important" }}
-          />
+          <p>Total unique movies in all collections:</p>
+          <p className={classes.moviesNum}>{totalUniqueMovies.length}</p>
         </Card>
-      )}
+        <Card className={classes.card}>
+          <p>Movies in current collection:</p>
+          <p className={classes.moviesNum}>{movies.length}</p>
+        </Card>
+      </div>
+      <CollapsingContent
+        title="Movies Genres Chart"
+        className={classes.collapsingWrapper}
+      >
+        <p className={classes.chartTitle}>Movies By Genres</p>
+        {genresChartData && (
+          <Bar options={barChartOptions} data={genresChartData} />
+        )}
+      </CollapsingContent>
+
+      <CollapsingContent
+        title="Movies Ratings Chart"
+        className={classes.collapsingWrapper}
+      >
+        <p className={classes.chartTitle}>Movies By Rating</p>
+        {ratingsChartData && (
+          <Bar options={barChartOptions} data={ratingsChartData} />
+        )}
+      </CollapsingContent>
+
+      <CollapsingContent
+        title="Movies Release Year Chart"
+        className={classes.collapsingWrapper}
+      >
+        <p className={classes.chartTitle}>Movies By Release Year</p>
+        {releaseYearChartData && (
+          <Bar options={barChartOptions} data={releaseYearChartData} />
+        )}
+      </CollapsingContent>
     </AuthPageWrapper>
   )
 }
